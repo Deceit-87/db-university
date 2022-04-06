2916b36d217f2a745cf3448bafa68250739917d2
Deceit-87 <manuelfreund318@gmail.com>
2022-04-06 21:18:05 +0200

    snak 1,2,3,

---
+++ join_06_04_22.sql
@@ -0,0 +1,67 @@
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+    SELECT `degrees`.`name`,`students`.`name`,`students`.`surname
+    FROM `degrees`
+    INNER JOIN `students`
+    ON `students`.`degree_id` = `degrees`.`id`
+    WHERE `degrees`.`name` = 'corso di laurea in economia';
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+    SELECT `degrees`.`name`
+    FROM `degrees`
+    INNER JOIN `departments`
+    ON `degrees`.`department_id` = `departments`.`id`
+    WHERE `departments`.`name`= 'Dipartimento di Neuroscienze';
+
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+    SELECT `courses`.`name`,`teachers`.`name`,`teachers`.`surname`
+    FROM `courses`
+    INNER JOIN `course_teacher`
+    ON `course_teacher`.`course_id` = `courses`.`id`
+    INNER JOIN `teachers`
+    ON `teachers`.`id` = `course_teacher`.`teacher_id`
+    WHERE `teachers`.`id` = 44;
+
+
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+---------------------------------------------------------------------------------------------------
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
